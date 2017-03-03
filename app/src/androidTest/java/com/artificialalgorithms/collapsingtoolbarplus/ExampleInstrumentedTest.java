99e8eac853885881b92ea0b9dd51e7d906cc89e9
Jeffrey Huang <Jeffhuang1@users.noreply.github.com>
2017-03-03 10:14:25 -0800

    Add Circle CI Integration (#1)

--- app/src/androidTest/java/com/artificialalgorithms/collapsingtoolbarplus/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/artificialalgorithms/collapsingtoolbarplus/ExampleInstrumentedTest.java
@@ -2,8 +2,12 @@ package com.artificialalgorithms.collapsingtoolbarplus;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.view.WindowManager;
 
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -16,6 +20,22 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+  @Rule
+  public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
+          MainActivity.class);
+
+  @Before
+  public void setUp() {
+    final MainActivity activity = mActivityRule.getActivity();
+    Runnable wakeUpDevice = new Runnable() {
+      public void run() {
+        activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON |
+                WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
+                WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+      }
+    };
+    activity.runOnUiThread(wakeUpDevice);
+  }
   @Test
   public void useAppContext() throws Exception {
     // Context of the app under test.
